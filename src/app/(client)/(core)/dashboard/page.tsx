c5d796cc6a0182e2ae2c2d81fa1409a27b942bc0
Debora Serra <debora.r.serra@gmail.com>
2025-08-30 10:50:14 -0700

    feat: add initial onboarding logic

--- src/app/(client)/(core)/dashboard/page.tsx
+++ src/app/(client)/(core)/dashboard/page.tsx
@@ -1,15 +1,39 @@
+'use client';
+
+import { useEffect, useState } from 'react';
+
+import { Dialog, DialogContent } from '@/shadcn-ui';
+
 import DashboardContent from './components/DashboardContent';
+import Onboarding from './components/Onboarding';
 
 export default function DashboardPage() {
+	const [openOnboarding, setOpenOnboarding] = useState(false);
+
+	useEffect(() => {
+		const apiKeys = [];
+		if (apiKeys.length === 0) {
+			setOpenOnboarding(true);
+		}
+	}, []);
+
 	return (
 		<>
 			<div className='px-6 py-10 sm:px-20'>
-				<DashboardContent />
+				{openOnboarding ? null : <DashboardContent />}
+				<Dialog
+					open={openOnboarding}
+					onOpenChange={setOpenOnboarding}
+				>
+					<DialogContent>
+						<Onboarding setIsOpen={setOpenOnboarding} />
+					</DialogContent>
+				</Dialog>
 			</div>
 		</>
 	);
 }
-export const metadata = {
-	title: 'Dashboard',
-	description: 'Your dashboard overview',
-};
+// export const metadata = {
+// 	title: 'Dashboard',
+// 	description: 'Your dashboard overview',
+// };
